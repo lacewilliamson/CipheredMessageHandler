47bf33efe9d76f1bff7e61c100cadd8f0ad6e752
Lace Williamson <lacew@protonmail.com>
2020-05-19 21:34:39 -0400

    Updated README, edited encipher method, created decipher method

--- CipheredMessageHandler/Program.cs
+++ CipheredMessageHandler/Program.cs
@@ -7,23 +7,32 @@ namespace CipheredMessageHandler
     {
         static void Main(string[] args)
         {
-            while (true)
-            { 
-            Console.WriteLine("Enter character to convert: ");
-            char userChar = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            Console.WriteLine("Enter one-time pad character: ");
-            char otpChar = Console.ReadKey().KeyChar;
-            Console.WriteLine();
+            string userInputPlain;
+            string userInputOTP;
+            string encipheredString = "";
 
-            encipher(userChar, otpChar);
-            Console.ReadLine();
-            }  
+            Console.WriteLine("Enter string to convert:");
+            userInputPlain = Console.ReadLine();
+            Console.WriteLine("Enter one time pad string: ");
+            userInputOTP = Console.ReadLine();
+            //userInputPlain = "MAYIM";
+            //userInputOTP = "LACEW";
+            userInputPlain = prepareForHandling(userInputPlain);
+            userInputOTP = prepareForHandling(userInputOTP);
+
+                for (int i = 0; i < userInputPlain.Length; i++)
+                {
+                    encipheredString += (Encipher(userInputPlain[i], userInputOTP[i]));
+                }
+                Console.WriteLine(encipheredString);
+                userInputPlain = "";
+                userInputOTP = "";
+                encipheredString = "";
         }
-        static void encipher (Char userCharacter, char oneTimePadCharacter)
+        static char Encipher (Char userCharacter, char oneTimePadCharacter)
         {
-            int userCharacterInt = 0;
-            int padCharacterInt = 0;
+            int userCharacterInt;
+            int padCharacterInt;
             userCharacterInt = userCharacter - 65; //This is done to make the math simpler to figure out, from ASCII to base 26 systems.
             padCharacterInt = oneTimePadCharacter - 65;  //This is done to make the math simpler to figure out, from ASCII to base 26 systems.
             int dencipheredCharacter = userCharacterInt + padCharacterInt;
@@ -31,13 +40,34 @@ namespace CipheredMessageHandler
                 {
                     dencipheredCharacter += 26;
                 }
-                if (dencipheredCharacter > 26)
+                if (dencipheredCharacter >= 26)
                 {
                     dencipheredCharacter -= 26;
                 }
-            Console.WriteLine($"Enciphered character =  {(char)(dencipheredCharacter +65)}.");
-                
-         
+            return ((char)(dencipheredCharacter + 65));
+        }
+        static char Decipher(Char userCharacter, char oneTimePadCharacter)
+        {
+            int userCharacterInt;
+            int padCharacterInt;
+            userCharacterInt = userCharacter - 65; //This is done to make the math simpler to figure out, from ASCII to base 26 systems.
+            padCharacterInt = oneTimePadCharacter - 65;  //This is done to make the math simpler to figure out, from ASCII to base 26 systems.
+            int dencipheredCharacter = userCharacterInt - padCharacterInt;
+            if (dencipheredCharacter < 0)
+            {
+                dencipheredCharacter += 26;
+            }
+            if (dencipheredCharacter > 26)
+            {
+                dencipheredCharacter -= 26;
+            }
+            return (char)(dencipheredCharacter + 65);
+        }
+        
+        static string prepareForHandling(string userString)
+        {
+            userString.Replace(" ", "");
+            return userString;
         }
     }
 }
